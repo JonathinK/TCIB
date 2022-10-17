f8b4d9ea8e2f8969445f0be70e54ad043e1c33d6
JonathinK <jonathin.kish@gmail.com>
2022-10-17 00:37:31 -0400

    change file paths and file names

--- gatsby-config.js
+++ gatsby-config.js
@@ -141,7 +141,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/Logo-Final.png`, // This path is relative to the root of the site.
+        icon: `src/images/Logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
